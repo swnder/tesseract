2ad3fcdf76b2517d05edd714b439f6e03f6f8f0c
swnder <sandrojir@gmail.com>
2024-04-24 19:18:34 -0400

    Se ignora la carpeta target

--- src/main/java/com/tesseract/tesseract/PDFImageToText.java
+++ src/main/java/com/tesseract/tesseract/PDFImageToText.java
@@ -12,7 +12,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PDFImageToText {
-
+    
     public static void extractTextFromPDF(String pdfPath) {
         try {
             PDDocument document = PDDocument.load(new File(pdfPath));
@@ -20,16 +20,21 @@ public class PDFImageToText {
             Tesseract tesseract = new Tesseract();
             tesseract.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata"); // Cambiar la ruta según tu configuración
             
+            String text = "";
+            
             for (int page = 0; page < document.getNumberOfPages(); ++page) {
                 BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300);
                 File tempFile = File.createTempFile("pdf-image-", ".png");
                 ImageIO.write(bim, "png", tempFile);
-                String text = tesseract.doOCR(tempFile);
+                text = tesseract.doOCR(tempFile); // Extrae el texto y lo guarda en la variable text
+                // visualizamos el contenido
                 System.out.println("Texto extraído de la página " + (page + 1) + ": \n" + text);
                 System.out.println(".....PAGINA SIGUIENTE.....\n ");
+                             
+                
                 tempFile.delete(); // Borra el archivo temporal creado
             }
-            
+            System.out.println("obtenemos: "+text);
             document.close();
         } catch (IOException | TesseractException e) {
             e.printStackTrace();
--- src/main/java/com/tesseract/tesseract/TesseractWrapper.java
+++ src/main/java/com/tesseract/tesseract/TesseractWrapper.java
@@ -3,7 +3,8 @@ package com.tesseract.tesseract;
 public class TesseractWrapper {
 
     public static void main(String[] args) {
-        String pdfPath = "D:\\RegimenMaquila\\Proyectos\\usandoTesseract\\data\\factura\\204696_FACT_EXP_SERV_404_TELEC.pdf"; // Cambiar la ruta del PDF
+        //String pdfPath = "D:\\RegimenMaquila\\Proyectos\\usandoTesseract\\data\\factura\\204696_FACT_EXP_SERV_404_TELEC.pdf"; // Cambiar la ruta del PDF
+        String pdfPath = "D:\\RegimenMaquila\\Proyectos\\usandoTesseract\\data\\facturas_maquila\\MQLIMT-FACT_COMER-209163-1.pdf";
         PDFImageToText.extractTextFromPDF(pdfPath);
     }
 }
